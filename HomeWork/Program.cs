dddf7dc910c0d91828a325eea3d7820b0d07c7fe
Mary <mariyastch@gmail.com>
2023-09-22 00:27:52 +0300

    1st task done

---
+++ HomeWork/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int MiddleDigit(int num)
+{
+    int dec = num / 10;
+    int ed = dec % 10;
+    return ed;
+}
+
+int NumLength(int num)
+{
+    int count = 0;
+    //int i = num;
+    while(num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Insert three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine(NumLength(num));
+
+while(NumLength(num) != 3)
+{
+    Console.Write("Insert THREE-DIGIT number: "); 
+    num = Convert.ToInt32(Console.ReadLine());  
+}
+ Console.WriteLine($"{num} -> {MiddleDigit(num)}");
+
+
