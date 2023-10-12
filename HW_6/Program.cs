6cceb4640926e75de326745e4d8aa271f54861e1
Mary <mariyastch@gmail.com>
2023-10-13 02:08:49 +0300

    modified

--- HW_6/Program.cs
+++ HW_6/Program.cs
@@ -2,11 +2,81 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+void PrintArray(int[] array)
+{
+    System.Console.Write("\n[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine("]");
+}
+
+// System.Console.WriteLine("Input array size: ");
+// int size = int.Parse(Console.ReadLine());
+// int[] array = new int[size];
+
+System.Console.WriteLine("Input array : ");
+string[] input = Console.ReadLine();
+// // int result = Convert.ToInt32(input);
+return input;
+System.Console.WriteLine(input);
+
+// var array = input.Split(' ').Select(Int32.Parse).ToArray();
+// // int size = array.Length
+// int[] arr = new int[array.Length];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     arr[i] = array[i];
+// }
+// return arr;
+
+// int CountPositive(var array)
+// {
+//     int count=0;
+//     for(int i=0;i<array.Length;i++)
+//     {
+//         if(array[i]>0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
 
 
+// PrintArray(array);
+// CountPositive(array);
+// System.Console.WriteLine($" -> {count}");
 
+// int countPositive(int[] array)
+// {
+//   int count = 0;
+//   for (int i = 0; i < array.Length; i++)
+//   {
+//     if (array[i] > 0)
+//       count++;
+//   }
+//   return count;
+// }
 
+// int[] inputInt(int M)
+// {
+//   int[] array = new int[M];
+//   for (int i = 0; i < M; i++)
+//   {
+//     Console.Write("Введите целое число: ");
+//     array[i] = int.Parse(Console.ReadLine());
+//   }
+//   return array;
+// }
 
+// Console.Write("Введите кол-во целых чисел: ");
+// int M = int.Parse(Console.ReadLine());
+// int[] array = inputInt(M);
+// Console.Write($"Ввели {countPositive(array)} положительных чисел");
+
+// PrintArray(arr);
 // _________________________________________________________________________________________________________________________
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
--- Sem_5/Program.cs
+++ Sem_5/Program.cs
@@ -3,58 +3,58 @@
 
 //Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
 
-// int[] CreateRandomArray()
-// {
-//     int[] array = new int[12];
-//     for (int i = 0; i < 12; i++)
-//     {
-//         array[i] = new Random().Next(-9, 10);
-//     }
-//     return array;
-// }
+int[] CreateRandomArray()
+{
+    int[] array = new int[12];
+    for (int i = 0; i < 12; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
 
-// void PrintArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i] + " ");
-//     }
-//     Console.WriteLine();
-// }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    // Console.WriteLine();
+}
 
-// int ArraySum(int[] array)
-// {
-//     int result = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         result = result + array[i];
-//     }
-//     return result;
-// }
+int ArraySum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = result + array[i];
+    }
+    return result;
+}
 
-// void PosNegSum(int[] array)
-// {
-//     int pos = 0;
-//     int neg = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i] > 0)
-//         {
-//             pos += array[i];
-//         }
-//         else
-//         {
-//             neg +=array[i];
-//         }
-//     }
-//     System.Console.WriteLine($"Sum of positive array elements -> {pos}\nSum of negative array elements -> {neg}");
-// }
+void PosNegSum(int[] array)
+{
+    int pos = 0;
+    int neg = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            pos += array[i];
+        }
+        else
+        {
+            neg +=array[i];
+        }
+    }
+    System.Console.WriteLine($"Sum of positive array elements -> {pos}\nSum of negative array elements -> {neg}");
+}
 
-// int[] myArray = CreateRandomArray();
-// PrintArray(myArray);
+int[] myArray = CreateRandomArray();
+PrintArray(myArray);
 
-// // ArraySum(myArray);
-// PosNegSum(myArray);
+// ArraySum(myArray);
+PosNegSum(myArray);
 
 // ______________________________________________________________________________________________________________
 // *Задача 35:** Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива,
@@ -166,37 +166,37 @@
 
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.Write(array[i] + " ");
-    }
-}
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+// }
 
-int[] CreateRandomArray()
-{
-    int[] array = new int[10];
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(-10, 11);
-    }
-    return array;
-}
+// int[] CreateRandomArray()
+// {
+//     int[] array = new int[10];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-10, 11);
+//     }
+//     return array;
+// }
 
 
-int[] ChangePos2Neg(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] *= -1;
-    }
-    return array;
-}
+// int[] ChangePos2Neg(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] *= -1;
+//     }
+//     return array;
+// }
 
-int[] myArray = CreateRandomArray();
-System.Console.Write("Изначальный массив: -> ");
-PrintArray(myArray);
-System.Console.WriteLine();
-System.Console.Write("Зеркальный массив: -> ");
-PrintArray(ChangePos2Neg(myArray));
+// int[] myArray = CreateRandomArray();
+// System.Console.Write("Изначальный массив: -> ");
+// PrintArray(myArray);
+// System.Console.WriteLine();
+// System.Console.Write("Зеркальный массив: -> ");
+// PrintArray(ChangePos2Neg(myArray));
